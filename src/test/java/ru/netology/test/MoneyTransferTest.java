65faa6556ea506b66df1e45c6f96b398530ca44b
PershikovaEP <zem524@mail.ru>
2022-08-21 19:28:34 +0300

    delete beforeeach

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -14,11 +14,6 @@ import static java.lang.String.valueOf;
 
 class MoneyTransferTest {
 
-    @BeforeEach
-    public void setUp() {
-        Configuration.timeout = 15000;
-    }
-
     @Test
     void shouldTransferMoneyOnCard1WithCard2V1() {
 
@@ -151,11 +146,11 @@ class MoneyTransferTest {
         var dashboardPage = verificationPage.validVerify(verificationCode);
         var currentBalance2Card = DashboardPage.getCardBalance(DashboardPage.getId2());
         var currentBalance1Card = DashboardPage.getCardBalance(DashboardPage.getId1());
-        var sumTranslation = DashboardPage.getAmount(DashboardPage.getId1()) + "000"; //лимит баланса от 1 карты. сумма больше лимита
+        var sumTranslation = DashboardPage.getAmount(DashboardPage.getId1()) + "00"; //лимит баланса от 1 карты. сумма больше лимита
         var choosingACard = dashboardPage.choosingACardToTopUpYourBalance(DashboardPage.getId2()); //выбираем пополнение 2 карты
         var transfer = choosingACard.transfer(sumTranslation, 0); //пополнение с 1 карты
-        int balanceAfterTransfer2Card = currentBalance2Card + Integer.parseInt(sumTranslation.substring(0,(sumTranslation.length()-3)));
-        int balanceAfterTransfer1Card = currentBalance1Card - Integer.parseInt(sumTranslation.substring(0,(sumTranslation.length()-3)));
+        int balanceAfterTransfer2Card = currentBalance2Card + Integer.parseInt(sumTranslation.substring(0,(sumTranslation.length()-2)));
+        int balanceAfterTransfer1Card = currentBalance1Card - Integer.parseInt(sumTranslation.substring(0,(sumTranslation.length()-2)));
         transfer.chekingBalance(DashboardPage.getId2(), balanceAfterTransfer2Card);
         transfer.chekingBalance(DashboardPage.getId1(), balanceAfterTransfer1Card);
     }
